a75012e74fcce25adeadeed2aa22418406a0d2bd
Shivangbhatnagar237 <shivangb237@gmail.com>
2022-11-01 20:33:21 +0530

    first commit

---
+++ socketapi.js
@@ -0,0 +1,43 @@
+const io = require("socket.io")();
+
+const socketapi = {
+    io: io
+};
+
+let namearr = [];
+let ids = [];
+
+// Add your socket.io logic here!
+io.on("connection", function (socket) {
+    io.emit('onlineUsers', namearr)
+
+    socket.on('disconnect', () => {
+        let index = ids.indexOf(socket.id);
+        let dname = namearr[index];
+        io.emit('left', dname);
+        namearr.splice(index, 1);
+        ids.splice(index, 1);
+        io.emit('onlineUsers', namearr);
+    })
+
+    socket.on('newUser', (username) => {
+        namearr.push(username);
+        ids.push(socket.id);
+        io.emit('onlineUsers', namearr);
+        io.emit('joined', username);
+    })
+
+    socket.on('newMessage', (data) => {
+        let index = ids.indexOf(socket.id);
+        let user = namearr[index];
+        io.emit('newMessage', { data, user });
+    });
+
+    socket.on('typing', () => {
+        let index = ids.indexOf(socket.id);
+        let user = namearr[index];
+        socket.broadcast.emit('typing', { user });
+    });
+});
+
+module.exports = socketapi;
